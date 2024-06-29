90f6641b7553f7cb4d74984f9361f955f5fd2cdb
Sarfraz Ali <sarfrazmahi152@gmail.com>
2024-06-29 16:34:11 +0530

    Add files via upload

---
+++ POSTInvoice.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.postinvoice;
+
+/**
+ *
+ * @author Sarfraz_Mahi
+ */
+public class POSTInvoice {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
